30c4b57832104c5d52c6c8010741942d195de803
wenjie <linuxwenjie@gmail.com>
2025-11-13 21:41:12 -0800

    refactor(dbus): normalize naming to watchdog service/object/interface

--- src/dbus_client.py
+++ src/dbus_client.py
@@ -1,13 +1,12 @@
 import dbus
 
-
 bus = dbus.SessionBus()
-remote_object = bus.get_object('com.example.SampleService', '/com/example/SampleObject')
-remote_interface = dbus.Interface(remote_object, 'com.example.SampleInterface')
-response_a = remote_interface.method_a('Hello, Method A!')
+remote_object = bus.get_object('com.example.watchdogService', '/com/example/watchdogObject')
+remote_interface = dbus.Interface(remote_object, 'com.example.watchdog')
+response_a = remote_interface.watchdog_dbus_add('Hello, Method A!')
 print(response_a)
-response_b = remote_interface.method_b('Hello, Method B!')
+response_b = remote_interface.watchdog_dbus_kick('Hello, Method B!')
 print(response_b)
-response_c = remote_interface.method_c('Hello, Method C!')
+response_c = remote_interface.watchdog_dbus_settime('Hello, Method C!')
 print(response_c)
 
--- src/dbus_server.py
+++ src/dbus_server.py
@@ -4,29 +4,33 @@ from dbus.mainloop.glib import DBusGMainLoop
 from gi.repository import GObject
 
 
-class test(dbus.service.Object):
+class watchdog_dbus(dbus.service.Object):
     def __init__(self):
-        bus_name = dbus.service.BusName('com.example.SampleService', bus=dbus.SessionBus())
-        dbus.service.Object.__init__(self, bus_name, '/com/example/SampleObject')
+        bus_name = dbus.service.BusName('com.example.watchdogService', bus=dbus.SessionBus())
+        dbus.service.Object.__init__(self, bus_name, '/com/example/watchdogObject')
 
-    @dbus.service.method('com.example.SampleInterface', in_signature='s', out_signature='s')
-    def method_a(self, message):
-        print('Received message in Method A: %s' % message)
-        return 'Method A Received: %s' % message
+    @dbus.service.method('com.example.watchdog', in_signature='s', out_signature='s')
+    def watchdog_dbus_add(self, message):
+        print('watchdog_dbus_add: %s' % message)
+        return 'watchdog_dbus_add: Received: %s' % message
 
-    @dbus.service.method('com.example.SampleInterface', in_signature='s', out_signature='s')
-    def method_b(self, message):
-        print('Received message in Method B: %s' % message)
-        return 'Method B Received: %s' % message
+    @dbus.service.method('com.example.watchdog', in_signature='s', out_signature='s')
+    def watchdog_dbus_del(self, message):
+        print('watchdog_dbus_del: %s' % message)
+        return 'watchdog_dbus_del Received: %s' % message
 
-    @dbus.service.method('com.example.SampleInterface', in_signature='s', out_signature='s')
-    def method_c(self, message):
-        print('Received message in Method C: %s' % message)
-        return 'Method C Received: %s' % message
+    @dbus.service.method('com.example.watchdog', in_signature='s', out_signature='s')
+    def watchdog_dbus_kick(self, message):
+        print('watchdog_dbus_kick: %s' % message)
+        return 'watchdog_dbus_kick Received: %s' % message
 
+    @dbus.service.method('com.example.watchdog', in_signature='s', out_signature='s')
+    def watchdog_dbus_settime(self, message):
+        print('watchdog_dbus_settime: %s' % message)
+        return 'watchdog_dbus_settime Received: %s' % message
 
 DBusGMainLoop(set_as_default=True)
 loop = GObject.MainLoop()
-test()
+watchdog_dbus()
 loop.run()
 

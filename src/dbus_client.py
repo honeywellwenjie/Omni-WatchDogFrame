590a2297d3f62325ac33af523af3502bb1782f48
wenjie <linuxwenjie@gmail.com>
2025-11-13 21:02:04 -0800

    feat(dbus): extend dbus client/server with multiple methods
    replace single sample_method with method_a, method_b, and method_c for expanded
    dbus functionality. update server class and align client calls to support
    multi-method communication.

--- src/dbus_client.py
+++ src/dbus_client.py
@@ -4,6 +4,10 @@ import dbus
 bus = dbus.SessionBus()
 remote_object = bus.get_object('com.example.SampleService', '/com/example/SampleObject')
 remote_interface = dbus.Interface(remote_object, 'com.example.SampleInterface')
-response = remote_interface.sample_method('Hello, DBus!')
-print(response)
+response_a = remote_interface.method_a('Hello, Method A!')
+print(response_a)
+response_b = remote_interface.method_b('Hello, Method B!')
+print(response_b)
+response_c = remote_interface.method_c('Hello, Method C!')
+print(response_c)
 
--- src/dbus_server.py
+++ src/dbus_server.py
@@ -4,19 +4,29 @@ from dbus.mainloop.glib import DBusGMainLoop
 from gi.repository import GObject
 
 
-class Example(dbus.service.Object):
+class test(dbus.service.Object):
     def __init__(self):
         bus_name = dbus.service.BusName('com.example.SampleService', bus=dbus.SessionBus())
         dbus.service.Object.__init__(self, bus_name, '/com/example/SampleObject')
 
     @dbus.service.method('com.example.SampleInterface', in_signature='s', out_signature='s')
-    def sample_method(self, message):
-        print('Received message: %s' % message)
-        return 'Received: %s' % message
+    def method_a(self, message):
+        print('Received message in Method A: %s' % message)
+        return 'Method A Received: %s' % message
+
+    @dbus.service.method('com.example.SampleInterface', in_signature='s', out_signature='s')
+    def method_b(self, message):
+        print('Received message in Method B: %s' % message)
+        return 'Method B Received: %s' % message
+
+    @dbus.service.method('com.example.SampleInterface', in_signature='s', out_signature='s')
+    def method_c(self, message):
+        print('Received message in Method C: %s' % message)
+        return 'Method C Received: %s' % message
 
 
 DBusGMainLoop(set_as_default=True)
 loop = GObject.MainLoop()
-Example()
+test()
 loop.run()
 
